4ff754b6d5a8903a1b0a3dc5264498dbd7bcda49
turovaes <turovaes@gmail.com>
2023-02-27 16:12:43 +0200

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int m = ReadInt("Введите число M: ");
+int n = ReadInt("Введите число N: ");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountSumFromMtoN(m, n)}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountSumFromMtoN(int m, int n)
+{
+    if (m == n) return n;
+    else return n + CountSumFromMtoN(m, n - 1);
+}
