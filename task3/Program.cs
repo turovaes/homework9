10525ef00db1b14609f10ce4026bb030458d1159
turovaes <turovaes@gmail.com>
2023-02-27 16:15:39 +0200

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите два положительных числа: M и N.");
+int m = ReadInt("Введите число M: ");
+int n = ReadInt("Введите число N: ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
