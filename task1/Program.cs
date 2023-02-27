83b3d217cb5a596500be40abf4c6e894423320ba
turovaes <turovaes@gmail.com>
2023-02-27 16:08:26 +0200

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = ReadInt("Введите натуральное число ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Вы ввели отрицательное число или 0");
+}
+Console.WriteLine(Numbers(n, m));
+
+int Numbers(int n, int m)
+{
+    if (n == m)
+    {
+        return n;
+    }
+    else
+    {
+        Console.Write($"{Numbers(n, m + 1)}, ");
+    }
+    return m;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
